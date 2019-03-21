10af2193607ff3deb044cd4261ce7389746b9fa2
David <dbravo7@yahoo.com>
2019-03-21 12:39:25 -0400

    Populated grid with bombs in board.rb

--- board.rb
+++ board.rb
@@ -16,9 +16,22 @@ class Board
 
     def self.create_grid(size)
         empty_grid = Array.new(size) {Array.new(size)}
-        empty_grid
+        Board.rand_bomb_placement(empty_grid)
     end 
 
+    def self.rand_bomb_placement(bomb_grid) 
+        size = bomb_grid.length 
+        num_of_bombs = ((size * size) * 0.123).round 
+
+        while num_of_bombs >= 1
+            x, y = [rand(0..8), rand(0..8)]
+            if  bomb_grid[x][y] == nil
+                bomb_grid[x][y] = "*"
+                num_of_bombs -= 1
+            end 
+        end  
+        bomb_grid
+    end 
 
 
 
