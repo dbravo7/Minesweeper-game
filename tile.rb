f47870ad15260b8cfc265aab8df564ac48609ed5
David <dbravo7@yahoo.com>
2019-03-21 16:49:21 -0400

    Added several methods to call from board clas to reveal, check if bomb, and check if flagged

--- tile.rb
+++ tile.rb
@@ -3,13 +3,41 @@ require 'colorize'
 class Tile
 
     def initialize(square)
-        @tile = square
-        @value = square == "*" ? true : false 
+        @value = square
+        @bomb = square == "*" ? true : false 
+        @revealed = false 
+        @flagged = false 
     end 
 
     def bomb?
-        @value
+        @bomb
     end 
+
+    def flagged?
+        @flagged
+    end 
+
+    def flag
+        @flagged = true 
+    end 
+
+    def unflag
+        @flagged = false
+    end 
+
+    def reveal 
+        @revealed = true 
+    end 
+
+    def revealed?
+        @revealed
+    end 
+
+    def to_value
+        revealed? ? @value : "?"
+    end 
+
+
 
     # have to check whether the tile is flagged
     # will have to reveal the tile
